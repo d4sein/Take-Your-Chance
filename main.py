8ef2e9527d733efc6fd8ed62687f70b13944078e
Willian Nascimento <45379088+d4sein@users.noreply.github.com>
2019-08-04 00:07:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-import random
-import time
+from random import randint
 
-
-random.seed(time.time())
-
-
-def func(chance: float) -> bool:
-	return random.random() <= chance
+def func(chance: int) -> int:
+	return randint(0, 100) <= chance
 
 
 for i in range(10):
-	result = [func(0.5) for i in range(1000000)]
+	result = [func(50) for i in range(1000000)]
 	print(f'True: {result.count(True) / 10000}')
 	print(f'False: {result.count(False) / 10000}')
 	print('-' * 20)
-  
+	
